3c0e31c14c090b2c96dac17f4d38fb7a1d712c46
anastasia-dorfman <anastasia.dorfman1@gmail.com>
2024-09-24 12:32:24 -0300

    customFetch updated

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -2,11 +2,7 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_API_URL || "/api";
 
 const customFetch = axios.create({
-<<<<<<< HEAD
-  baseURL: import.meta.env.VITE_API_URL || "/api",
-=======
   baseURL,
->>>>>>> main
   withCredentials: true,
 });
 export default customFetch;
